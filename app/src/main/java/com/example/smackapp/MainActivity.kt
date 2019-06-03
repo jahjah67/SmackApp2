769a82cca1f7b631449feefb8b0f52ebd6edda0f
jahjah67 <changszehong93@gmail.com>
2019-06-03 21:26:01 +0800

    Login Screen UI and Navigation Header Designs

--- app/src/main/java/com/example/smackapp/MainActivity.kt
+++ app/src/main/java/com/example/smackapp/MainActivity.kt
@@ -11,6 +11,7 @@ import android.support.design.widget.NavigationView
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.Menu
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
@@ -38,5 +39,16 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    fun loginBtnNavClicked(view: View){
+
+    }
+
+    fun addChannelClicked(view: View){
+
+    }
+
+    fun sendMsgBtnClick(view: View){
+
+    }
 
 }
